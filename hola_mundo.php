1c8a47a93935e59e270b137b7971427607fb8c69
AurumJMS <125156701+AurumJMS@users.noreply.github.com>
2023-02-11 20:19:04 +0000

    Update hola_mundo.php
    modificacion desde cuenta 2 para ejercicio 12

--- hola_mundo.php
+++ hola_mundo.php
@@ -3,7 +3,7 @@ echo "Hola Mundo";
 
 echo "Se añade sgunda linea al archivo segun ejercicio numero 8";
 
-
+echho "Segun ejercicio 12, modifico archivo desde cuenta 2 para luego pull request";
 
 
 ?>
